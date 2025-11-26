8d97ad060e9eb4503ede04ba14cd3d8224ba91d5
ayushkukshal <ayushkukshal@gmail.com>
2025-11-26 21:43:02 +0530

    addlec3

---
+++ lec3.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout<<"Enter your input:"<<endl;
+    cin>>x;
+    cout<<"Your input is :"<<x<<endl;
+    return 0;
+}
