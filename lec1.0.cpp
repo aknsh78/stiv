2b9f43d553318c13084c5dd04c69d5031e9242d9
ayushkukshal <ayushkukshal@gmail.com>
2025-11-26 20:31:55 +0530

    Initial commit

---
+++ lec1.0.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    std::cout<<"Hey Stiver!"<<std::endl<<"i am good"<< endl;
+    cout<<"Hey Stiver!"<<endl;
+    cout<<"Hey Stiver!"<<"\n";
+    cout<<"Hey Stiver!"<<endl;
+    int x,y;
+    cin>>x>>y;
+    cout<<"value:"<<x<<"and"<<y<<endl;;
+    cout<<"value:"<<x;
+    cout<<"and"<<y;
+    return 0;
+
+} 
